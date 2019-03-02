6ac96fa41ab61b34c62bfb772e663f14c719cd3a
RicardRuiz <rruizcas12@alumnes.ub.edu>
2019-03-02 22:33:27 +0100

    P1 & P2 Practicas PAE.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/69/8017e87cc039001916acc067c0bf9af2
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+Practica_02\ PAE_Alumnes.obj: ../Practica_02\ PAE_Alumnes.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include" --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/ricard/Dropbox/Universidad/Asignaturas/2018-2019/Programació d'Arquitectures Encastades/CCSWS/P2-PORTS" --include_path="/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="Practica_02 PAE_Alumnes.d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+startup_msp432p401r_ccs.obj: ../startup_msp432p401r_ccs.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include" --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/ricard/Dropbox/Universidad/Asignaturas/2018-2019/Programació d'Arquitectures Encastades/CCSWS/P2-PORTS" --include_path="/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="startup_msp432p401r_ccs.d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+system_msp432p401r.obj: ../system_msp432p401r.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include" --include_path="/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS" --include_path="/home/ricard/Dropbox/Universidad/Asignaturas/2018-2019/Programació d'Arquitectures Encastades/CCSWS/P2-PORTS" --include_path="/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="system_msp432p401r.d_raw" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
---
+++ P2-PORTS/Debug/Practica_02 PAE_Alumnes.d
@@ -0,0 +1,43 @@
+# FIXED
+
+Practica_02\ PAE_Alumnes.obj: ../Practica_02\ PAE_Alumnes.c
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/msp432p401r.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h
+Practica_02\ PAE_Alumnes.obj: /home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h
+Practica_02\ PAE_Alumnes.obj: ../lib_PAE2.h
+
+../Practica_02\ PAE_Alumnes.c: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+/home/ricard/ti/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h: 
+/home/ricard/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h: 
+../lib_PAE2.h: 
